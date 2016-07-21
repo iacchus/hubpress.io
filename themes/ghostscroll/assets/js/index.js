91ada708428ae747cb5992e7c68df82415978834
Iacchus Mercurius <kassivs@gmail.com>
2016-07-21 14:31:51 -0300

    freak

--- themes/ghostscroll/assets/js/index.js
+++ themes/ghostscroll/assets/js/index.js
@@ -3,11 +3,9 @@
  */
 
 var $post = $('.post');
-//var $first = $('.post.first'); 
-var $first = $('article.first'); 
+var $first = $('.post.first'); 
 var $last = $('.post.last'); 
 var $fnav = $('.fixed-nav');
-var $fnavc = $('.fixed-nav > .fixed-nav-container');
 var $postholder = $('.post-holder');
 var $postafter = $('.post-after');
 var $sitehead = $('#site-head');
@@ -20,16 +18,25 @@ var $sitehead = $('#site-head');
 			scrollTop: el.offset().top
 		}, 1000);
 	}
-
-
 	$(document).ready(function(){
-
-		// FitVids for responsive videos
-		$('.post-content').fitVids();
-
-		$('header').click( function () {
-			srcTo($first);
+        
+        // FitVids for responsive videos
+        $('.post-content').fitVids();
+	 
+		$postholder.each(function (e) {
+			if(e % 2 != 0)
+				$(this).addClass("odd");
 		});
+
+		$postafter.each(function (e) {
+			var bg = $(this).parent().css('background-color')
+			$(this).css('border-top-color', bg);
+
+			if (e % 2 == 0) {
+				$(this).addClass("even");
+			}
+		});
+		
 		$('.btn.first').click( function () {
 			srcTo($first);
 		});
@@ -39,22 +46,22 @@ var $sitehead = $('#site-head');
 		$('#header-arrow').click(function () {
 			srcTo($first);
 		});
-		$('#totop-arrow').click(function () {
-			srcTo($sitehead);
-		});
+        $('#totop-arrow').click(function () {
+            srcTo($sitehead);
+        });
 
 		$('.post-title').each(function () {
 			var t = $(this).text();
 			var index = $(this).parents('.post-holder').index();
-			$fnavc.append("<a class='fn-item' item_index='"+index+"'>"+t+"</a>");
+			$fnav.append("<a class='fn-item' item_index='"+index+"'>"+t+"</a>")
 			$(this).parents('article').attr('id',t.toLowerCase().split(' ').join('-'));
-			$('.fn-item').click(function () {
+			$('.fn-item').last().click(function () {
 				var i = $(this).attr('item_index');
 				var s = $(".post[item_index='"+i+"']");
 
 				$('html, body').animate({
 					scrollTop: s.offset().top
-				}, 400);
+				}, 0);
 
 			});
 		});
@@ -64,11 +71,16 @@ var $sitehead = $('#site-head');
 			$(window).scroll( function () {
 				var w = $(window).scrollTop();
 				var g = $sitehead.offset().top;
-				var h = $sitehead.offset().top + $(this).height()-100;
-
+				var h = $sitehead.offset().top + $sitehead.height()-100;
+				
+				/*var paralex = 30 + w/13 + "%";
+    			$sitehead.css("background-position-y", paralex); */
+                
+    			$sitehead.css("background-position-y");
+                
 				if(w >= g && w<=h) {
-					$('.fixed-nav').fadeOut('slow');
-				} else if($(window).width()>1130) {
+					$('.fixed-nav').fadeOut('fast');
+				} else if($(window).width()>500) {
 					$('.fixed-nav').fadeIn('slow');
 				}
 
@@ -92,8 +104,28 @@ var $sitehead = $('#site-head');
 			});
 		}
 
+		$('ul li').before('<span class="bult fa fa-asterisk icon-asterisk"></span>');
+		$('blockquote p').prepend('<span class="quo icon-quote-left"></span>');
+		$('blockquote p').append('<span class="quo icon-quote-right"></span>');
 	});
+	
+	$post.each(function () {
+		var postText = $(this).html();
+		var fa  = [];
+		for(var i=0; i < icons.length; i++) {
+			fa[i]       = {};
+			fa[i].str   = "@"+ icons[i]+ "@";
+			fa[i].icon  = icons[i];
+			fa[i].int   = postText.search(fa[i].str);
 
-
+			if(fa[i].int > -1 ) { 
+				fa[i].count = postText.match(new RegExp(fa[i].str,"g")).length;
+				for(var j=0; j < fa[i].count; j++) {
+					$(this).html($(this).html().replace(fa[i].str, "<i class='fa "+fa[i].icon+"'></i>"))
+				}
+			}
+		}
+	});
+	
 
 }(jQuery));
