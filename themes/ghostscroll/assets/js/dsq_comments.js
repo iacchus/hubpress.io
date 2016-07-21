cdbf5799fc26ea41afa7d416db2d9969677b5d48
Iacchus Mercurius <kassivs@gmail.com>
2016-07-21 10:07:58 -0300

    other person s analytics and discuss code removed

--- themes/ghostscroll/assets/js/dsq_comments.js
+++ themes/ghostscroll/assets/js/dsq_comments.js
@@ -1,4 +1,4 @@
-    var disqus_shortname = 'twelvepurplepills'; // required: replace example with your forum shortname
+    var disqus_shortname = ''; // required: replace example with your forum shortname
     /* * *  var disqus_identifier = '{{post.id}}'; // make sure to use the post.id as an identifier, otherwise disqus will use the pages url per default, which might be problematic... * * */
 
     /* * * DON'T EDIT BELOW THIS LINE * * */
