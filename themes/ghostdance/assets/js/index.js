e722f3e77983a915c2dfade0cc39cfbd692a4da5
Iacchus Mercurius <kassivs@gmail.com>
2016-08-03 14:07:33 -0300

    ipynb-html styling...

--- themes/ghostdance/assets/js/index.js
+++ themes/ghostdance/assets/js/index.js
@@ -39,7 +39,7 @@ var $authorhead = $('#author-head');
 			$.get(myurl,
 				function(response) {
 					//$(el).contents().find("body").append("<style type=\"text/css\">body{padding:33px;}</style>");
-					$(el).contents().find("head").append('<link rel="stylesheet" href="' + mycsss +'/ipynb-custom.css" />'); // FIXME!!
+					$(el).contents().find("body").append('<link rel="stylesheet" href="' + mycsss +'/ipynb-custom.css" />'); // FIXME!!
 					$(el).contents().find('body').append(response);
 					$(el).height( $(el).contents().height() );	
 				},
